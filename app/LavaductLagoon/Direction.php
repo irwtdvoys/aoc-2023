098e544cfaa06dea3a98b37319049680f5c17366
Steve Adams <irwtdvoys@gmail.com>
2023-12-18 21:54:12 +0000

    Implement tasks

---
+++ app/LavaductLagoon/Direction.php
@@ -0,0 +1,13 @@
+<?php
+	declare(strict_types=1);
+
+	namespace App\LavaductLagoon;
+
+	enum Direction: string
+	{
+		case UP = "U";
+		case RIGHT = "R";
+		case DOWN = "D";
+		case LEFT = "L";
+	}
+?>
